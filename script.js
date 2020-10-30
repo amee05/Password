8c16ae287fa8f3d25796ba1a283b890c0db55b1b
Amee Parmar <amee05@yahoo.com>
2020-10-30 13:04:20 -0700

    code

--- script.js
+++ script.js
@@ -13,32 +13,42 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+let Length = prompt('How many charactoers do you want in your password?')
+let maxLength = document.getElementById(Length);
+
+// for (var l = 0; l< maxLength; l++)
 // Add random uppercase alphabet
-let maxLength = prompt('How many charactoers do you want in your password?')
-let maxLength = document.getElementById(maxLength);
-let up = prompt('Do you want uppercase alphabet in your password?')
-If up = true
+let up = confirm('Do you want uppercase alphabet in your password?')
+console.log(up = true);
+if (up === true)
+{
 function upperCase() {
   var upper = "";
   var Cap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-  for (var i = 0; i < maxLength; i++)
+  for (var i = 0; i < 1; i++)
     upper += Cap.charAt(Math.floor(Math.random() * Cap.length));
 
   return upper;
 }
 
 console.log(upperCase());
-
-// Add random lowercase alphabet
-function lowerCase() {
-  var lower = "";
-  var Cap = "abcdefghijklmnopqrstuvwxyz";
-
-  for (var i = 0; i < 5; i++)
-    lower += Cap.charAt(Math.floor(Math.random() * Cap.length));
-
-  return lower;
 }
 
-console.log(lowerCase());
+// Add random lowercase alphabet
+// let lower = confirm('Do you want uppercase alphabet in your password?')
+// // If up = 'true'
+
+// Add random lowercase alphabet
+// function lowerCase() {
+//   var lower = "";
+//   var Cap = "abcdefghijklmnopqrstuvwxyz";
+
+//   for (var i = 0; i < 1; i++)
+//     lower += Cap.charAt(Math.floor(Math.random() * Cap.length));
+
+//   return lower;
+// }
+
+// console.log(lowerCase());
